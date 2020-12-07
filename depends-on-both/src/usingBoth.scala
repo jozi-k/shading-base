29f6ac935004213548a34fa8c6ba1effd5df4d8a
Jozef Kovaľ <jozef.koval@avast.com>
2020-12-07 01:08:22 +0100

    Fix typo in method name.

--- depends-on-both/src/usingBoth.scala
+++ depends-on-both/src/usingBoth.scala
@@ -3,7 +3,7 @@ package jozinek
 object Main {
     def main(args: Array[String]): Unit = {
         
-        println(s"v1 tag says even is: ${UsingV1.isEven}")
+        println(s"v1 tag says even is: ${UsingV1.isEvenV1}")
         //println(s"v2 tag says odd is: ${UsingV2.isOdd}")
     }
 }
