d272deb5574e41b4f77599d5215d0ffefe321192
Jozef Kovaľ <jozef.koval@avast.com>
2020-12-07 01:25:33 +0100

    Use v2 via dependency.

--- depends-on-both/src/usingBoth.scala
+++ depends-on-both/src/usingBoth.scala
@@ -2,8 +2,7 @@ package jozinek
 
 object Main {
     def main(args: Array[String]): Unit = {
-        
         println(s"v1 tag says even is: ${UsingV1.isEvenV1}")
-        //println(s"v2 tag says odd is: ${UsingV2.isOdd}")
+        println(s"v2 tag says odd is: ${UsingV2.isOddV2}")
     }
 }
